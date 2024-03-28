22a41abd5150eb050a01a93f0a93827bc7f124dd
Ankit Sen <ankitsenchowdhury@gmail.com>
2024-03-28 14:10:18 +0530

    Updated

--- src/main/java/com/crio/qeats/repositories/RestaurantRepository.java
+++ src/main/java/com/crio/qeats/repositories/RestaurantRepository.java
@@ -1,2 +1,11 @@
-package com.crio.qeats.repositories;public class RestaurantRepository {
+package com.crio.qeats.repositories;
+
+import com.crio.qeats.models.RestaurantEntity;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface RestaurantRepository extends MongoRepository<RestaurantEntity, String> {
+
 }
